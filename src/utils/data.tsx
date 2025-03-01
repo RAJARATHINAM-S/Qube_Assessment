00ffd3e9fbf98bf9addc784e0cc294452682aa6e
Rajaratinam-s <rajarathinams.mech@gmail.com>
2025-03-01 22:37:39 +0530

    albums

---
+++ src/utils/data.tsx
@@ -0,0 +1,294 @@
+export const albums = [
+  {
+    id: 1,
+    collectionName: 'EPIC: The Troy Saga (Official Concept Album)',
+    artistName: 'Jorge Rivera-Herrans',
+    type: 'EP',
+    songCount: 5,
+    duration: '00:15:40',
+    size: '45 MB',
+    releasedOn: '03 Sept 2024, 02:35 PM',
+    songs: [
+      {
+        title: 'The Horse and the Infant',
+        performers:
+          'Jorge Rivera-Herrans, Luke Holt & Cast of EPIC: The Musical',
+        duration: '00:02:15',
+        size: '13 MB',
+      },
+      {
+        title: 'Just a Man',
+        performers: 'Jorge Rivera-Herrans & Cast of EPIC: The Musical',
+        duration: '00:02:30',
+        size: '18 MB',
+      },
+      {
+        title: 'Full Speed Ahead',
+        performers:
+          'Jorge Rivera-Herrans, Armando Julián, Steven Dookie & Cast of EPIC: The Musical',
+        duration: '00:05:10',
+        size: '24 MB',
+      },
+      {
+        title: 'Open Arms',
+        performers: 'Jorge Rivera-Herrans & Steven Dookie',
+        duration: '00:05:10',
+        size: '23 MB',
+      },
+      {
+        title: 'Warrior of the Mind',
+        performers:
+          'Jorge Rivera-Herrans, Teagan Earley & Cast of EPIC: The Musical',
+        duration: '00:05:10',
+        size: '23 MB',
+      },
+    ],
+  },
+  {
+    id: 2,
+    collectionName: 'EPIC: The Ocean Saga (Official Concept Album)',
+    artistName: 'Jorge Rivera-Herrans',
+    type: 'EP',
+    songCount: 4,
+    duration: '00:13:00',
+    size: '15 MB',
+    releasedOn: '04 Sept 2024, 10:00 AM',
+    songs: [
+      {
+        title: 'The Horse and the Infant',
+        performers:
+          'Jorge Rivera-Herrans, Luke Holt & Cast of EPIC: The Musical',
+        duration: '00:02:15',
+        size: '13 MB',
+      },
+      {
+        title: 'Just a Man',
+        performers: 'Jorge Rivera-Herrans & Cast of EPIC: The Musical',
+        duration: '00:02:30',
+        size: '18 MB',
+      },
+      {
+        title: 'Full Speed Ahead',
+        performers:
+          'Jorge Rivera-Herrans, Armando Julián, Steven Dookie & Cast of EPIC: The Musical',
+        duration: '00:05:10',
+        size: '24 MB',
+      },
+      {
+        title: 'Open Arms',
+        performers: 'Jorge Rivera-Herrans & Steven Dookie',
+        duration: '00:05:10',
+        size: '23 MB',
+      },
+    ],
+  },
+  {
+    id: 3,
+    collectionName: 'EPIC: The Ithaca Saga (Official Concept Album)',
+    artistName: 'Jorge Rivera-Herrans',
+    type: 'EP',
+    songCount: 4,
+    duration: '00:12:24',
+    size: '30 MB',
+    releasedOn: '04 Sept 2024, 10:00 AM',
+    songs: [
+      {
+        title: 'The Horse and the Infant',
+        performers:
+          'Jorge Rivera-Herrans, Luke Holt & Cast of EPIC: The Musical',
+        duration: '00:02:15',
+        size: '13 MB',
+      },
+      {
+        title: 'Just a Man',
+        performers: 'Jorge Rivera-Herrans & Cast of EPIC: The Musical',
+        duration: '00:02:30',
+        size: '18 MB',
+      },
+      {
+        title: 'Full Speed Ahead',
+        performers:
+          'Jorge Rivera-Herrans, Armando Julián, Steven Dookie & Cast of EPIC: The Musical',
+        duration: '00:05:10',
+        size: '24 MB',
+      },
+      {
+        title: 'Open Arms',
+        performers: 'Jorge Rivera-Herrans & Steven Dookie',
+        duration: '00:05:10',
+        size: '23 MB',
+      },
+    ],
+  },
+  {
+    id: 4,
+    collectionName: 'Collection Name',
+    artistName: 'Artist Name',
+    type: 'Album',
+    songCount: 8,
+    duration: '00:21:06',
+    size: '12 MB',
+    releasedOn: '10 Oct 2024, 02:35 PM',
+    songs: [
+      {
+        title: 'The Horse and the Infant',
+        performers:
+          'Jorge Rivera-Herrans, Luke Holt & Cast of EPIC: The Musical',
+        duration: '00:02:15',
+        size: '13 MB',
+      },
+      {
+        title: 'Just a Man',
+        performers: 'Jorge Rivera-Herrans & Cast of EPIC: The Musical',
+        duration: '00:02:30',
+        size: '18 MB',
+      },
+      {
+        title: 'Full Speed Ahead',
+        performers:
+          'Jorge Rivera-Herrans, Armando Julián, Steven Dookie & Cast of EPIC: The Musical',
+        duration: '00:05:10',
+        size: '24 MB',
+      },
+      {
+        title: 'Open Arms',
+        performers: 'Jorge Rivera-Herrans & Steven Dookie',
+        duration: '00:05:10',
+        size: '23 MB',
+      },
+      {
+        title: 'The Horse and the Infant',
+        performers:
+          'Jorge Rivera-Herrans, Luke Holt & Cast of EPIC: The Musical',
+        duration: '00:02:15',
+        size: '13 MB',
+      },
+      {
+        title: 'Just a Man',
+        performers: 'Jorge Rivera-Herrans & Cast of EPIC: The Musical',
+        duration: '00:02:30',
+        size: '18 MB',
+      },
+      {
+        title: 'Full Speed Ahead',
+        performers:
+          'Jorge Rivera-Herrans, Armando Julián, Steven Dookie & Cast of EPIC: The Musical',
+        duration: '00:05:10',
+        size: '24 MB',
+      },
+      {
+        title: 'Open Arms',
+        performers: 'Jorge Rivera-Herrans & Steven Dookie',
+        duration: '00:05:10',
+        size: '23 MB',
+      },
+    ],
+  },
+  {
+    id: 5,
+    collectionName: 'Collection Name',
+    artistName: 'Artist Name',
+    type: 'Album',
+    songCount: 7,
+    duration: '00:20:22',
+    size: '10 MB',
+    releasedOn: '01 Sept 2024, 12:31 AM',
+    songs: [
+      {
+        title: 'The Horse and the Infant',
+        performers:
+          'Jorge Rivera-Herrans, Luke Holt & Cast of EPIC: The Musical',
+        duration: '00:02:15',
+        size: '13 MB',
+      },
+      {
+        title: 'Just a Man',
+        performers: 'Jorge Rivera-Herrans & Cast of EPIC: The Musical',
+        duration: '00:02:30',
+        size: '18 MB',
+      },
+      {
+        title: 'Full Speed Ahead',
+        performers:
+          'Jorge Rivera-Herrans, Armando Julián, Steven Dookie & Cast of EPIC: The Musical',
+        duration: '00:05:10',
+        size: '24 MB',
+      },
+      {
+        title: 'Open Arms',
+        performers: 'Jorge Rivera-Herrans & Steven Dookie',
+        duration: '00:05:10',
+        size: '23 MB',
+      },
+      {
+        title: 'The Horse and the Infant',
+        performers:
+          'Jorge Rivera-Herrans, Luke Holt & Cast of EPIC: The Musical',
+        duration: '00:02:15',
+        size: '13 MB',
+      },
+      {
+        title: 'Just a Man',
+        performers: 'Jorge Rivera-Herrans & Cast of EPIC: The Musical',
+        duration: '00:02:30',
+        size: '18 MB',
+      },
+      {
+        title: 'Full Speed Ahead',
+        performers:
+          'Jorge Rivera-Herrans, Armando Julián, Steven Dookie & Cast of EPIC: The Musical',
+        duration: '00:05:10',
+        size: '24 MB',
+      },
+    ],
+  },
+  {
+    id: 6,
+    collectionName: 'Collection Name',
+    artistName: 'Artist Name',
+    type: 'Album',
+    songCount: 0,
+    duration: '00:25:40',
+    size: '16 MB',
+    releasedOn: '05 Sept 2024, 12:31 AM',
+    songs: [],
+  },
+  {
+    id: 7,
+    collectionName: 'Collection Name',
+    artistName: 'Artist Name',
+    type: 'Single',
+    songCount: 1,
+    duration: '00:01:20',
+    size: '24 MB',
+    releasedOn: '11 Oct 2024, 12:31 AM',
+    songs: [
+      {
+        title: 'The Horse and the Infant',
+        performers:
+          'Jorge Rivera-Herrans, Luke Holt & Cast of EPIC: The Musical',
+        duration: '00:02:15',
+        size: '13 MB',
+      },
+    ],
+  },
+  {
+    id: 8,
+    collectionName: 'Collection Name',
+    artistName: 'Artist Name',
+    type: 'Single',
+    songCount: 1,
+    duration: '00:01:20',
+    size: '24 MB',
+    releasedOn: '11 Oct 2024, 12:31 AM',
+    songs: [
+      {
+        title: 'The Horse and the Infant',
+        performers:
+          'Jorge Rivera-Herrans, Luke Holt & Cast of EPIC: The Musical',
+        duration: '00:02:15',
+        size: '13 MB',
+      },
+    ],
+  },
+];
