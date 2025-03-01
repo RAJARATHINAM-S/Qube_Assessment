bb74df3e2d02926b75d88d479066f01b82c662df
Rajaratinam-s <rajarathinams.mech@gmail.com>
2025-03-01 21:59:51 +0530

    prime

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './app-router';
-import './assets/css/index.css';
-import 'primereact/resources/themes/lara-light-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
+
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
+import './assets/css/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/themes/lara-light-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
